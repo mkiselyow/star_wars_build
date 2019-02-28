4f0791fe56e7b7f25bdcd7899c4ece7305bfa5c5
mkyse <mkyse@softserveinc.com>
2019-02-28 12:40:05 +0200

    first commit

---
+++ precache-manifest.07c8912d77cf14155f1145469f745a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "87520128dbc2485c5b69a15d5a65940d",
+    "url": "/static/media/satan_hum_sav.87520128.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a4492cb499021052ba9",
+    "url": "/static/js/main.3a4492cb.chunk.js"
+  },
+  {
+    "revision": "770e40c501493aff766b",
+    "url": "/static/js/1.770e40c5.chunk.js"
+  },
+  {
+    "revision": "3a4492cb499021052ba9",
+    "url": "/static/css/main.2f84a08f.chunk.css"
+  },
+  {
+    "revision": "434263cc4171d2b834197b5ebff9b48a",
+    "url": "/index.html"
+  }
+];
